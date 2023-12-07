262516c9ffdeda42078d332dd0d7b06709e2888e
sachi0601 <sachio.exe.uver@gmail.com>
2023-12-07 16:19:29 +0900

    add instance secgroup & userdata

--- main.tf
+++ main.tf
@@ -5,8 +5,32 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_instance" "test" {
-  ami = "ami-035d55281a86f9439"
+resource "aws_instance" "test-userdata" {
+  ami = "ami-0f7b55661ecbbe44c"
   instance_type = "t2.micro"
   availability_zone = "ap-northeast-1c"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "Hello World" > index.html
+  nohup busybox httpd -f -p 8080 &
+  EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    name="terraform-test"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "test-userdata-secgp"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
